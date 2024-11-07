d2f43aa213774a37d88822a0e81f5ec3ff502372
kirkyboi <136046535+kirkyboi@users.noreply.github.com>
2024-11-07 15:20:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CRUD</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<div>
+    <label>First Name:</label>
+    <input type = "text" name ="firstname">
+</div>
+
+<div>
+    <label>Middle Name:</label>
+    <input type = "text" name ="middlename">
+</div>
+
+<div>
+    <label>Last Name:</label>
+    <input type = "text" name ="lastname">
+</div>
+
+<div>
+    <label>Age:</label>
+    <input type = "text" name ="age">
+</div>
+
+<div>
+    <label>Address:</label>
+    <input type = "text" name ="address">
+</div>
+
+<div>
+    <label>Course&Section:</label>
+    <input type = "text" name ="course&section">
+</div>
+</body>
+</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
