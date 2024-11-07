d8d585df8d71549704f0f09842d690203998e098
kirkyboi <136046535+kirkyboi@users.noreply.github.com>
2024-11-07 15:40:52 +0800

    index.php

--- index.php
+++ index.php
@@ -1,44 +1,57 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP CRUD</title>
+    <title>MY FIRST UBUNTU PROJECT DEPLOYMENT</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style1.css">
 </head>
 <body>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <div class="container mt-5">
+        <p>MY FIRST UBUTN PROJECT DEPLOYMENT</p>
+        <form action="submit.php" method="POST">
+            <!-- First Name -->
+            <div class="mb-3">
+                <label for="firstname" class="form-label">First Name:</label>
+                <input type="text" class="form-control" id="firstname" name="firstname" required>
+            </div>
 
-<div>
-    <label>First Name:</label>
-    <input type = "text" name ="firstname">
-</div>
+            <!-- Middle Name -->
+            <div class="mb-3">
+                <label for="middlename" class="form-label">Middle Name:</label>
+                <input type="text" class="form-control" id="middlename" name="middlename">
+            </div>
 
-<div>
-    <label>Middle Name:</label>
-    <input type = "text" name ="middlename">
-</div>
+            <!-- Last Name -->
+            <div class="mb-3">
+                <label for="lastname" class="form-label">Last Name:</label>
+                <input type="text" class="form-control" id="lastname" name="lastname" required>
+            </div>
 
-<div>
-    <label>Last Name:</label>
-    <input type = "text" name ="lastname">
-</div>
+            <!-- Age -->
+           <div class="mb-3">
+                <label for="lastname" class="form-label">Age:</label>
+                <input type="text" class="form-control" id="Age" name="lastname" required>
+            </div>
 
-<div>
-    <label>Age:</label>
-    <input type = "text" name ="age">
-</div>
+            <!-- Address -->
+            <div class="mb-3">
+                <label for="address" class="form-label">Address:</label>
+                <input type="text" class="form-control" id="address" name="address" required>
+            </div>
 
-<div>
-    <label>Address:</label>
-    <input type = "text" name ="address">
-</div>
+            <!-- Course & Section -->
+            <div class="mb-3">
+                <label for="course_section" class="form-label">Course & Section:</label>
+                <input type="text" class="form-control" id="course_section" name="course_section" required>
+            </div>
 
-<div>
-    <label>Course&Section:</label>
-    <input type = "text" name ="course&section">
-</div>
+            <!-- Submit Button -->
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
